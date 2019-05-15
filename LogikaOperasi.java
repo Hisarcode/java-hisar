35408b319082b7a37157c1452385f8aafe9c75ff
Hisarcode <35738606+Hisarcode@users.noreply.github.com>
2019-05-15 13:44:49 +0700

    membuat LogikaOperasi.java

---
+++ LogikaOperasi.java
@@ -0,0 +1,11 @@
+public class LogikaOperasi{
+	public static void main(String[] args){
+		int x = 10 , y = 5;
+		System.out.println("x > y :  " + (x > y));
+		System.out.println("x < y :  " + (x  <  y));
+		System.out.println("x >= y : " + (x >= y ));
+		System.out.println("x <= y : " + (x <= y ));
+		System.out.println("x == y : " + (x == y ));
+		System.out.println("x != y : " + (x != y ));
+	}
+}
